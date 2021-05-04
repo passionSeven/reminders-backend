0c4e9e8cd3422705c5ae05e5799788be7df35abd
passionSeven <angelvsdevil312@gmail.com>
2021-05-03 22:32:53 -0400

    Enable cors to accept requests from any site

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const isProduction = process.env.NODE_ENV === "production";
 const origin = {
   origin: isProduction ? "https://fullchee-reminders.netlify.app" : "*",
 };
-app.use(cors(origin));
+app.use(cors());
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
