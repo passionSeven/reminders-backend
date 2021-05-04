bd7682e8bce0db49ff2312fc21a444fa35be4a3b
passionSeven <angelvsdevil312@gmail.com>
2021-05-03 23:32:22 -0400

    Reject unauthorized to be false

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const connectionString = `{process.env.DATABASE_URL}`
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: false,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = { pool };
